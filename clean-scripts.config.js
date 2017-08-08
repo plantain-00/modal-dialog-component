f3df6964cd4820444e4c1bef03b8dcc1e5d949df
york yao <yaoao12306@outlook.com>
2017-08-08 20:11:13 +0800

    improve scripts

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -1,29 +1,36 @@
 module.exports = {
   build: [
-    `rimraf demo/**/index.bundle-*.js demo/*.bundle-*.css`,
     `rimraf dist/`,
-    `file2variable-cli src/vue.template.html -o src/vue-variables.ts --html-minify --base src`,
-    `tsc -p src`,
-    `tsc -p demo`,
-    `lessc src/modal-dialog.less > dist/modal-dialog.css`,
-    `cleancss -o dist/modal-dialog.min.css dist/modal-dialog.css`,
-    `cleancss -o demo/index.bundle.css dist/modal-dialog.min.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`,
-    `webpack --display-modules --config demo/webpack.config.js`,
+    `mkdirp dist/`,
+    {
+      js: [
+        `file2variable-cli src/vue.template.html -o src/vue-variables.ts --html-minify --base src`,
+        `tsc -p src`,
+        `tsc -p demo`,
+        `webpack --display-modules --config demo/webpack.config.js`
+      ],
+      css: [
+        `lessc src/modal-dialog.less > dist/modal-dialog.css`,
+        `cleancss -o dist/modal-dialog.min.css dist/modal-dialog.css`,
+        `cleancss -o demo/index.bundle.css dist/modal-dialog.min.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`
+      ],
+      clean: `rimraf demo/**/index.bundle-*.js demo/*.bundle-*.css`
+    },
     `rev-static --config demo/rev-static.config.js`
   ],
-  lint: [
-    `tslint "src/**/*.ts" "src/**/*.tsx" "spec/**/*.ts" "demo/**/*.ts" "demo/**/*.tsx"`,
-    `standard "**/*.config.js"`,
-    `stylelint "src/**/*.less"`
-  ],
+  lint: {
+    ts: `tslint "src/**/*.ts" "src/**/*.tsx" "spec/**/*.ts" "demo/**/*.ts" "demo/**/*.tsx"`,
+    js: `standard "**/*.config.js"`,
+    less: `stylelint "src/**/*.less"`
+  },
   test: [
     'tsc -p spec',
     'karma start spec/karma.config.js'
   ],
-  fix: [
-    `standard --fix "**/*.config.js"`
-  ],
-  release: [
-    `clean-release`
-  ]
+  fix: {
+    ts: `tslint --fix "src/**/*.ts" "src/**/*.tsx" "spec/**/*.ts" "demo/**/*.ts" "demo/**/*.tsx"`,
+    js: `standard --fix "**/*.config.js"`,
+    less: `stylelint --fix "src/**/*.less"`
+  },
+  release: `clean-release`
 }
